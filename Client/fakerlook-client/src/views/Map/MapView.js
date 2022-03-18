4573275f5b26c394f82670a3d5aa8830505e7ffd
Liad Dadon <80317730+liaddx1@users.noreply.github.com>
2022-03-18 02:41:26 +0200

    Map working (just a map)

--- Client/fakerlook-client/src/views/Map/MapView.js
+++ Client/fakerlook-client/src/views/Map/MapView.js
@@ -1,9 +1,29 @@
 import React from "react";
-import { GoogleMap } from "react-google-maps";
-const MapView = props => {
-    return (
-        <GoogleMap defaultZoom={10} defaultCenter={{ lat: 31.726870, lng: 34.992470 }} />
-    );
+import { InfoWindow, useLoadScript, GoogleMap, Marker, } from "@react-google-maps/api";
+
+const libraries = ["places"];
+const mapContainerStyle = {
+    width: `100vw`,
+    height: `100vh`,
+}
+const center = {
+    lat: 31.726870,
+    lng: 34.992470,
 }
 
-export default MapView;
+export default function MapView() {
+    const { isLoaded, loadError } = useLoadScript({
+        googleMapsApiKey: 'AIzaSyAj0XIcENjviv-vfhvA1slF3mpdrARUAic',
+        libraries,
+    })
+
+    if (loadError) return "Error Loading Maps";
+    if (!isLoaded) return "Loadng Maps";
+    return (
+        <GoogleMap
+            mapContainerStyle={mapContainerStyle}
+            zoom={8}
+            center={center}
+        ></GoogleMap>
+    );
+}
